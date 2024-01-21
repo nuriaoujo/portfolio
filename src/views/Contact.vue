5474d7e46760f8b8fe56095ae2976ea3f80b2641
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-01-21 20:26:37 +0100

    contact page modified

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,38 +1,26 @@
 <template>
   <div>
     
-    <h1>Let's get in touch</h1>
+    <h1>¡Conectemos!</h1>
 
-    <div style="margin-bottom:40px;">I'm looking for a job !<br/>Feel free to contact me about any opportunity, or just to chat about cooking, kittens or anything else.</div>
+    <div style="margin-bottom:40px;">
+      Actualmente estoy en búsqueda de empleo,<br/>
+      Estoy explorando nuevas oportunidades profesionales y me encantaría formar parte de vuestro equipo. <br/>
+      Si considerais que mi perfil puede contribuir a vuestros proyectos... ¡No dudéis en contactar conmigo! <br/>
+      ¡Gracias por visitar mi portfolio!</div>
 
     <ul>
       <li>
-        <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
-        <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
-      </li>
-      <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
-        <a href="mailto:my@email.com" target="_blank">my@email.com</a>
+        <a href="mailto:oujomiguez.nuria@gmail.com" target="_blank">oujomiguez.nuria@gmail.com</a>
       </li>
       <li>
         <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
-        <a href="https://www.linkedin.com/me" target="_blank">linkedin.com/me</a>
+        <a href="https://www.linkedin.com/in/nuriaoujo/" target="_blank">linkedin.com/in/nuriaoujo/</a>
       </li>
       <li>
         <i class="fa fa-github fa-lg fa-fw"></i>
-        <a href="https://github.com/me" target="_blank">github.com/me</a>
-      </li>
-      <li>
-        <i class="fa fa-gamepad fa-lg fa-fw"></i>
-        <a href="https://myself.itch.io" target="_blank">myself.itch.io</a>
-      </li>
-      <li>
-        <i class="fa fa-tumblr fa-lg fa-fw"></i>
-        <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
-      </li>
-      <li>
-        <i class="fa fa-steam fa-lg fa-fw"></i>
-        <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
+        <a href="https://github.com/nuriaoujo" target="_blank">github.com/nuriaoujo</a>
       </li>
     </ul>
 
