982a4004a16ca9069782b4dce318c6dc21e9dd81
Nuria Oujo <nuriaoujo.n1@gmail.com>
2024-01-22 21:33:55 +0100

    img inserted into fullstack app descriptions

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -47,6 +47,7 @@ export default [
         </div>
     </div>
     `, "#DAB88B", true),
+
     new ProjectData("project-2", "ProductsApp", "img/projects/project-2.png", `
     <div class="paragraph">
         <strong>Products App</strong> es el primer proyecto Full Stack que he realizado dentro del Bootcamp en <strong>Introducción a la Programación</strong>.
@@ -71,22 +72,21 @@ export default [
         <div class="slide-wrapper">
              
             <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img3.png" alt="iDERS Screenshot" />
+                <img class="slide-number" src="img/projects/web/productsapp/img2.png" alt="iDERS Screenshot" />
             </div>
 
             <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img2.png" alt="iDERS Screenshot" />
+                <img class="slide-number" src="img/projects/web/productsapp/img4.png" alt="iDERS Screenshot" />
             </div>
 
             <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img1.png" alt="iDERS Screenshot" />
+                <img class="slide-number" src="img/projects/web/productsapp/img3.png" alt="iDERS Screenshot" />
             </div>
 
         </div>
     </div>
-
-    
     `, "#DAB88B"),
+
     new ProjectData("project-3", "Gifs App", "img/projects/project-3-icon.png", `
     <div class="paragraph">
         <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
@@ -98,24 +98,11 @@ export default [
         </div>
     </div>
 
-    <div id="slideshow">
-        <div class="slide-wrapper">
-             
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img3.png" alt="iDERS Screenshot" />
-            </div>
-
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img2.png" alt="iDERS Screenshot" />
-            </div>
-
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img1.png" alt="iDERS Screenshot" />
-            </div>
-
-        </div>
+    <div class="paragraph">
+        <img class="slide-number" src="img/projects/web/whegym/img3.png" alt="iDERS Screenshot" />
     </div>
     `, "#DAB88B"),
+
     new ProjectData("project-4", "TODO App", "img/projects/project-4.png", `
     <div class="paragraph">
     <strong>TODO App</strong> es un proyecto creado en el curso de JavaScript moderno de dev/talles, donde se elabora
@@ -133,25 +120,12 @@ export default [
         Visualizar demo en <a href="https://nuriaoujo.github.io/todo-app/" target="_blank">GitHub Pages</a>.
         </div>
     </div>
-
-    <div id="slideshow">
-        <div class="slide-wrapper">
              
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img3.png" alt="iDERS Screenshot" />
-            </div>
-
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img2.png" alt="iDERS Screenshot" />
-            </div>
-
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img1.png" alt="iDERS Screenshot" />
-            </div>
-
-        </div>
+    <div class="paragraph">
+        <img class="slide-number" src="img/projects/web/extras/todoapp-img.png" alt="iDERS Screenshot" />
     </div>
     `, "#DAB88B"),
+
     new ProjectData("project-5", "BlackJack", "img/projects/project-5.png", `
     <div class="paragraph">
         <strong>BlackJack</strong> es un proyecto realizado en el curso de dev/talles con la finalidad de practicar sobre JavaScript.
@@ -167,22 +141,8 @@ export default [
         </div>
     </div>
 
-    <div id="slideshow">
-        <div class="slide-wrapper">
-             
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img3.png" alt="iDERS Screenshot" />
-            </div>
-
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img2.png" alt="iDERS Screenshot" />
-            </div>
-
-            <div class="slide">
-                <img class="slide-number" src="img/projects/web/whegym/img1.png" alt="iDERS Screenshot" />
-            </div>
-
-        </div>
+    <div class="paragraph">
+        <img class="slide-number" src="img/projects/web/extras/blackjack-img.png" alt="iDERS Screenshot" />
     </div>
     `, "#DAB88B")
 ];
